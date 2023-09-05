a2a1210e8a17fc9ae61c7581d2268cf83d8076df
Avinash kumar chaurasia <87019999+avinashhere017@users.noreply.github.com>
2023-09-05 17:43:02 +0530

    Update 1378. Replace Employee ID With The Unique Identifier.sql

--- Basic Joins/1378. Replace Employee ID With The Unique Identifier.sql
+++ Basic Joins/1378. Replace Employee ID With The Unique Identifier.sql
@@ -1,3 +1,6 @@
 # Write your MySQL query statement below
 
-select EmployeeUNI.unique_id, Employees.name from Employees LEFT JOIN EmployeeUNI on EmployeeUNI.id = Employees.id;
+# select EmployeeUNI.unique_id , Employees.name from Employees left join EmployeeUNI on EmployeeUNI.id = Employees.id;
+
+
+select empu.unique_id ,e.name from Employees as e left join EmployeeUNI as empu on empu.id = e.id;
